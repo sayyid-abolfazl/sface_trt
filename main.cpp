8372138d320f34b36cc50049242b40123fbd9f40
sayyid-abolfazl <sayyid.1378.2@gmail.com>
2025-02-04 09:51:05 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 #include <string>
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
+#include <vector>
+#include <cmath>
+#include <numeric>
+#include <iostream>
 
 #include "sface.hpp"
 
@@ -14,10 +17,7 @@
 
 
 
-#include <vector>
-#include <cmath>
-#include <numeric>
-#include <iostream>
+
 
 // روش اول: استفاده از فرمول استاندارد کسینوسی
 float cosineSimilarityStandard(const std::vector<float>& vec1, const std::vector<float>& vec2) {
@@ -149,9 +149,49 @@ float normalizedCorrelation(const std::vector<float>& vec1, const std::vector<fl
     return numerator / std::sqrt(denominator1 * denominator2);
 }
 
-// 
 
-// 
+
+class YuNet
+{
+  public:
+    YuNet(const std::string& model_path,
+          const cv::Size& input_size,
+          const float conf_threshold,
+          const float nms_threshold,
+          const int top_k,
+          const int backend_id,
+          const int target_id)
+    {
+        _detector = cv::FaceDetectorYN::create(
+            model_path, "", input_size, conf_threshold, nms_threshold, top_k, backend_id, target_id);
+    }
+
+    void setInputSize(const cv::Size& input_size)
+    {
+        _detector->setInputSize(input_size);
+    }
+
+    void setTopK(const int top_k)
+    {
+        _detector->setTopK(top_k);
+    }
+
+    cv::Mat infer(const cv::Mat& image)
+    {
+        cv::Mat result;
+        _detector->detect(image, result);
+        return result;
+    }
+
+  private:
+    cv::Ptr<cv::FaceDetectorYN> _detector;
+};
+
+
+
+
+
+
 
 int main(int argc, char **argv) {
     cudaSetDevice(DEVICE);
@@ -163,6 +203,30 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+
+    
+    // https://github.com/opencv/opencv_zoo/tree/main/models/face_detection_yunet
+    auto face_detector = YuNet(
+    "face_detection_yunet_2023mar.onnx", cv::Size(320, 320), 0.7 , 0.3 , 1000, cv::dnn::DNN_BACKEND_CUDA, cv::dnn::DNN_TARGET_CUDA);
+
+
+
+    
+    const std::vector<std::pair<int, int>> backend_target_pairs = {
+        {cv::dnn::DNN_BACKEND_OPENCV, cv::dnn::DNN_TARGET_CPU},
+        {cv::dnn::DNN_BACKEND_CUDA,   cv::dnn::DNN_TARGET_CUDA},
+        {cv::dnn::DNN_BACKEND_CUDA,   cv::dnn::DNN_TARGET_CUDA_FP16},
+        {cv::dnn::DNN_BACKEND_TIMVX,  cv::dnn::DNN_TARGET_NPU},
+        {cv::dnn::DNN_BACKEND_CANN,   cv::dnn::DNN_TARGET_NPU}
+        };
+
+
+
+
+
+
+
+
     const std::string engine_file_path = argv[1];
     const std::string q = argv[3];
     const std::string t = argv[4];
@@ -170,13 +234,147 @@ int main(int argc, char **argv) {
 
 
 
-    sface sample = sface(engine_file_path);
+    sface sface_obj = sface(engine_file_path);
 
 
-    cv::Mat target = cv::imread("111.jpg");
-    cv::Mat query = cv::imread("444.jpg");
-    std::vector<float> vec_s1 = sample.infer(target);
-    std::vector<float> vec_s2 = sample.infer(query);
+    cv::Mat target = cv::imread(t);
+    cv::Mat query = cv::imread(q);
+    cv::imshow("target",target);
+    cv::imshow("query",query);
+    cv::waitKey(0);
+
+
+
+
+
+    static std::vector<cv::Scalar> landmark_colors{
+        cv::Scalar(255, 0, 0),   // right eye
+        cv::Scalar(0, 0, 255),   // left eye
+        cv::Scalar(0, 255, 0),   // nose tip
+        cv::Scalar(255, 0, 255), // right mouth corner
+        cv::Scalar(0, 255, 255)  // left mouth corner
+    };
+
+
+
+    
+    // Detect single face in target image
+    face_detector.setInputSize(query.size());
+    face_detector.setTopK(1000);
+    cv::Mat faces_query = face_detector.infer(query);
+
+
+
+
+    cv::Mat  face_crop_query;
+
+    if (faces_query.total() > 0 && faces_query.cols >= 15) {
+        float x = faces_query.at<float>(0, 0);
+        float y = faces_query.at<float>(0, 1);
+        float w = faces_query.at<float>(0, 2);
+        float h = faces_query.at<float>(0, 3);
+        float confidence = faces_query.at<float>(0, 14);
+
+        cv::Rect2f original_face_box(x, y, w, h);
+
+        // تنظیم مستطیل به اندازه تصویر
+        original_face_box &= cv::Rect2f(0, 0, query.cols, query.rows);
+
+        if (original_face_box.width > 0 && original_face_box.height > 0) {
+            face_crop_query = query(original_face_box);
+            
+
+            // رسم مستطیل روی تصویر اصلی
+            cv::rectangle(query, original_face_box, cv::Scalar(0, 255, 0), 2);
+
+            // محاسبه لندمارک‌ها
+            std::vector<cv::Point2f> original_landmarks(5);
+            for (int j = 0; j < 5; ++j) {
+                float original_x = faces_query.at<float>(0, 4 + 2 * j);
+                float original_y = faces_query.at<float>(0, 5 + 2 * j);
+
+                if (original_x >= 0 && original_x < query.cols &&
+                    original_y >= 0 && original_y < query.rows) {
+                    original_landmarks[j] = cv::Point2f(original_x, original_y);
+                    cv::circle(query, original_landmarks[j], 2, landmark_colors[j], -1);
+                }
+            }
+
+            cv::imshow("query", face_crop_query);
+            cv::waitKey(0);
+
+
+
+        } else {
+            std::cerr << "Invalid face box!" << std::endl;
+        }
+    } else {
+        std::cerr << "No face detected!" << std::endl;
+    }
+
+
+
+    face_detector.setInputSize(target.size());
+    face_detector.setTopK(1000);
+    cv::Mat faces_target = face_detector.infer(target);
+
+
+    cv::Mat face_crop_target;
+
+    if (faces_target.total() > 0 && faces_target.cols >= 15) {
+        float x = faces_target.at<float>(0, 0);
+        float y = faces_target.at<float>(0, 1);
+        float w = faces_target.at<float>(0, 2);
+        float h = faces_target.at<float>(0, 3);
+        float confidence = faces_target.at<float>(0, 14);
+
+        cv::Rect2f original_face_box(x, y, w, h);
+
+        // تنظیم مستطیل به اندازه تصویر
+        original_face_box &= cv::Rect2f(0, 0, target.cols, target.rows);
+
+        if (original_face_box.width > 0 && original_face_box.height > 0) {
+            face_crop_target = target(original_face_box);
+
+            // رسم مستطیل روی تصویر اصلی
+            cv::rectangle(target, original_face_box, cv::Scalar(0, 255, 0), 2);
+
+            // محاسبه لندمارک‌ها
+            std::vector<cv::Point2f> original_landmarks(5);
+            for (int j = 0; j < 5; ++j) {
+                float original_x = faces_target.at<float>(0, 4 + 2 * j);
+                float original_y = faces_target.at<float>(0, 5 + 2 * j);
+
+                if (original_x >= 0 && original_x < target.cols &&
+                    original_y >= 0 && original_y < target.rows) {
+                    original_landmarks[j] = cv::Point2f(original_x, original_y);
+                    cv::circle(target, original_landmarks[j], 2, landmark_colors[j], -1);
+                }
+            }
+
+            cv::imshow("target", face_crop_target);
+            cv::waitKey(0);
+
+
+
+        } else {
+            std::cerr << "Invalid face box!" << std::endl;
+        }
+    } else {
+        std::cerr << "No face detected!" << std::endl;
+    }
+
+
+
+
+
+    std::vector<float> vec_s1 = sface_obj.infer(face_crop_query);
+    std::vector<float> vec_s2 = sface_obj.infer(face_crop_target);
+
+
+
+    //
+
 
     // بردارهای ویژگی نمونه
     std::vector<float> vec_onnx2 = {
@@ -207,40 +405,46 @@ int main(int argc, char **argv) {
         -1.3055    , -2.01115   ,  0.26059034,  1.0996912 , -1.8880942 ,
         -0.6834755 , -0.53702193,  1.458859  , -0.88288474, -0.5808352 ,
         0.61602205,  0.64711094,  0.05647586
-                
+                        
     
-    }; // 128 ویژگی
+    }; 
 
     std::vector<float> vec_onnx1 = {
         
-        -1.7179822 ,  0.6564609 , -1.0647364 , -0.70782745, -0.14286849,
-        -0.10902017, -1.3021411 , -0.33553702,  1.0994564 , -0.66910225,
-        0.05673549,  1.9792243 ,  1.0743947 , -0.21698561,  0.6745935 ,
-        0.54311633,  0.7595187 , -0.38423416,  0.03660099, -0.49724004,
-        0.5056483 ,  2.1010637 , -1.4586555 ,  0.8838126 ,  0.1918744 ,
-        -1.8427391 ,  0.05449065, -0.2661895 ,  0.48029292,  1.5273198 ,
-        0.32496956,  1.8302406 , -0.78103656,  0.7674867 , -1.4364387 ,
-        0.43543446, -0.3235675 , -0.6117327 ,  0.9160278 , -2.3339293 ,
-        0.7782307 ,  0.03402354, -1.3195915 ,  0.56168467,  0.27369   ,
-        -0.813949  , -0.8264483 ,  0.529851  ,  0.3350946 ,  0.61294115,
-        1.2581551 , -0.31351095,  1.8375161 , -1.7013233 , -1.1085179 ,
-        -0.80826986,  0.42728865,  0.79817885, -0.9445191 ,  0.24375781,
-        -0.43600875, -1.0872229 , -0.37747476, -1.5050787 , -0.58066195,
-        -0.79607844,  0.9655771 ,  0.7575069 ,  1.1719381 ,  0.5031031 ,
-        -1.2840176 ,  0.19543956, -0.08010055,  1.3876262 ,  2.7067566 ,
-        -3.734546  ,  0.58853537, -0.8721188 ,  0.40296343,  0.06157439,
-        -1.169652  ,  0.21362472,  0.23140194,  1.1391896 ,  0.71354496,
-        0.52315104,  0.20296276,  0.25904092, -1.5249878 , -1.0655183 ,
-        0.03047626,  0.87265754,  1.4249079 ,  0.05582605, -0.73617584,
-        -0.80756515, -0.50670624,  0.45515552,  1.4988574 ,  0.8100662 ,
-        -0.12381744, -0.65324736, -2.0939891 , -0.22820728,  0.25445655,
-        -1.0035957 ,  1.2556089 , -0.03834247,  0.94604707,  0.5135448 ,
-        0.15220915, -1.7795776 ,  0.8268679 ,  0.66912365,  0.25936642,
-        -0.21946414,  0.04142863,  0.6880018 ,  0.87044007,  0.20214759,
-        -0.50374526,  0.7281609 , -0.63916385,  0.88356113, -0.39560845,
-        0.8856474 , -2.7403674 , -0.7886186
+        3.2790530e-01, -1.5365446e+00, -1.3242102e+00,  1.2409792e+00,
+        -2.7258158e-01, -2.0406487e+00,  4.9704936e-01,  1.9078279e+00,
+        -1.0051534e+00, -7.9201776e-01, -3.0298296e-01,  1.6219306e+00,
+        1.6060572e+00,  9.5780432e-01,  1.1979878e+00, -1.7008569e+00,
+        -8.2805490e-01, -1.3175385e+00,  2.1399561e-01,  6.9383204e-01,
+        -6.5262511e-02, -5.3778476e-01,  1.0744743e+00, -2.2741408e+00,
+        -2.7306964e+00,  4.9025226e-01, -7.3746783e-01,  3.6385757e-01,
+        5.3154302e-01, -7.7065915e-01,  4.7597572e-01, -4.8986650e-01,
+        -9.0130115e-01, -2.0708339e+00,  4.4152823e-01, -2.3998962e-01,
+        6.8964928e-02,  4.5314556e-01, -5.5004275e-01,  3.8459998e-01,
+        1.0561351e+00,  4.6152782e-01,  1.1736318e+00,  1.6482894e+00,
+        1.0772864e+00, -1.5275799e-01, -1.2691997e+00, -1.3586960e+00,
+        1.3368955e+00,  9.0550238e-01, -2.0675287e-01,  2.2661140e+00,
+        5.9332275e-01,  1.1782950e+00,  1.2461269e+00,  3.2238719e-01,
+        6.7247242e-01, -8.3125257e-01,  6.8761572e-02, -2.7081497e+00,
+        -7.6467299e-01,  7.2135109e-01, -1.5446901e+00, -4.0035555e-01,
+        -8.1679732e-02, -1.9907393e+00,  8.1078970e-01,  6.1983550e-01,
+        1.3161994e+00,  1.7966273e+00, -1.6318296e+00,  8.7770122e-01,
+        -9.4180292e-01,  3.7487742e-01, -5.1890320e-01, -2.2657459e+00,
+        -5.3627688e-01, -1.1255981e+00, -1.1895188e+00, -4.5786881e-01,
+        2.3753619e+00, -1.7044790e+00, -4.9245644e-01, -8.2061106e-01,
+        -2.4532299e+00,  7.5733435e-01,  9.5817554e-01, -9.3460781e-03,
+        -2.3034678e+00, -3.6875024e-01,  1.1810551e+00,  3.3984312e-01,
+        8.1700087e-01,  2.6740214e-01,  5.5383903e-01, -4.5838475e-01,
+        -6.5805435e-01,  1.7217162e+00, -6.1314893e-01,  5.1607120e-01,
+        9.5685345e-01, -2.3502645e+00, -1.3855140e+00, -9.5718163e-01,
+        -1.0941375e+00, -9.9444028e-04, -1.0826866e-01,  6.3040143e-01,
+        1.3179220e-01, -2.1676111e+00,  1.0939851e+00,  2.4895388e-01,
+        -1.4269400e+00,  5.1890349e-01, -8.8770592e-01, -7.5672638e-01,
+        4.5518523e-01, -1.1810297e+00,  5.4373610e-01, -2.1511212e-01,
+        -3.3364172e+00,  6.9221562e-01,  2.4890668e+00, -1.2017430e+00,
+        -2.3514113e-01,  1.2106668e+00, -2.4143462e+00,  7.0786905e-01
                     
-    }; // 128 ویژگی
+    }; 
 
     std::cout << "\n\n--------------------------- SFace ONNX demo.py : -------------------- " << "\n";
 
